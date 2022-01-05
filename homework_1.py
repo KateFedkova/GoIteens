b6bebc6f90351b69ef72e4bf30f922db8f0924cc
Kate Fedkova <kate.fedkova@gmail.com>
2022-01-05 12:59:36 +0200

    Create homework_1.py

---
+++ homework_1.py
@@ -0,0 +1,8 @@
+# відформатувати start_string щоб отримати наступні змінні = їх значення і вивести на екран
+start_string = "abcdefghijklmn"
+print(len(start_string))
+print(start_string[0:4])
+print(start_string[-5:14])
+print(start_string[5:9])
+print(start_string[:: 2])
+
