df343cd5d27a622139155e0c232fd190b0a39baa
Kate Fedkova <kate.fedkova@gmail.com>
2022-01-16 18:41:17 +0200

    Create homework_3.py

---
+++ homework_3.py
@@ -0,0 +1,48 @@
+# Task 1
+#Написати калькулятор для операцій + - * /
+
+print('Welcome to calculator!')
+open = input('''We have a few options: + - * /
+Choose the one you like: ''')
+if open == '+':
+    first = input('Enter first number: ')
+    if first.isdigit():
+        second = input('Enter second number: ')
+        if second.isdigit():
+            print(f"{first} + {second} = {int(first) + int(second)}")
+        else:
+            print(f"{second} isn't digit")
+    else:
+        print(f"{first} isn't digit") 
+elif open == '-':
+    first = input('Enter first number: ')
+    if first.isdigit():
+        second = input('Enter second number: ')
+        if second.isdigit():
+            print(f"{first} - {second} = {int(first) - int(second)}")
+        else:
+            print(f"{second} isn't digit")        
+    else:
+        print(f"{first} isn't digit")
+elif open == '*':
+    first = input('Enter first number: ')
+    if first.isdigit():
+        second = input('Enter second number: ')
+        if second.isdigit():
+            print(f"{first} * {second} = {int(first) * int(second)}")
+        else:
+            print(f"{second} isn't digit")        
+    else:
+        print(f"{first} isn't digit")
+elif open == '/':
+    first = input('Enter first number: ')
+    if first.isdigit():
+        second = input('Enter second number: ')
+        if second.isdigit():
+                print(f"{first} / {second} = {int(first) / int(second)}")
+        else:
+            print(f"{second} isn't digit")
+    else:
+        print(f"{first} isn't digit")    
+else:
+    print("Not valid operation")
