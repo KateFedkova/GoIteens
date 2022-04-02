2d1c92f072ee25308d50dfa61c886142e3eccdac
Kate Fedkova <kate.fedkova@gmail.com>
2022-04-02 18:42:57 +0300

    Update homework_20.py

--- homework_20.py
+++ homework_20.py
@@ -10,24 +10,6 @@ def hello(user_name):
 def user_name():
     user_name = input( "Enter your username: ")
     return user_name   
-    
-    
-def menu():
-    options = input("""
-    Ви можете:
-      Для продавців:
-                     - зареєструвати товар                   : add_func
-                     - видалити товар                        : del_func
-      Для покупців:
-                     - пошук товару за категоріями           : search_func
-                     - подивитися загальну кількість товарів : num_of_items_func
-                     - список всіх товарів                   : all_items
-                     - купити товар                          : buy_func
-                     - залишити відгук                       : review_func
-             
-     q - вийти
-     Оберіть: """)
-    return options 
 
     
 def storage_filling(list_storage):
@@ -45,7 +27,23 @@ class Store:
     
     def __init__(self, list_storage):
         self.list_storage = list_storage  
-        
+            
+    def menu(self):
+        options = input("""
+        Ви можете:
+         Для продавців:
+                           - зареєструвати товар                   : add_func
+                           - видалити товар                        : del_func
+         Для покупців:
+                           - пошук товару за категоріями           : search_func
+                           - подивитися загальну кількість товарів : num_of_items_func
+                           - список всіх товарів                   : all_items
+                           - купити товар                          : buy_func
+                           - залишити відгук                       : review_func
+                     
+        q - вийти
+        Оберіть: """)
+        return options     
     
     def characteristic(self):
         characteristic = input('''
@@ -60,17 +58,21 @@ class Store:
    
     def characteristic_desicion(self, characteristic):
         if characteristic == 'Папір офісний':
-            brand,colour,format_of_paper,num_of_paper,price = items.copy_paper()
-            return items.add_copy_paper(brand,colour,format_of_paper,num_of_paper,price)
+            brand,colour,format_of_paper,num_of_paper,price = self.copy_paper()
+            result_dict = self.add_copy_paper(brand,colour,format_of_paper,num_of_paper,price)
+            return self.add_to_list(result_dict)
         elif characteristic == 'Настільне приладдя':
-            brand_t,size,material,price_t = items.for_table()
-            return items.add_for_table(brand_t,size,material,price_t)
+            brand_t,size,material,price_t = self.for_table()
+            result_dict = self.add_for_table(brand_t,size,material,price_t)
+            return self.add_to_list(result_dict)
         elif characteristic == 'Письмове приладдя':
-            brand_w,diametr,colour_of,price_w,line = items.writing_instruments()
-            return items.add_writ_inst(brand_w,diametr,colour_of,price_w,line)
+            brand_w,diametr,colour_of,price_w,line = self.writing_instruments()
+            result_dict = self.add_writ_inst(brand_w,diametr,colour_of,price_w,line)
+            return self.add_to_list(result_dict)
         elif characteristic == 'Скотч, стрейч-плівка':
-            brand_s,size_s,wide,scale_s,colour_of_s,length,price_s = items.scotch()
-            return items.add_scotch(brand_s,size_s,wide,scale_s,colour_of_s,length,price_s)
+            brand_s,size_s,wide,scale_s,colour_of_s,length,price_s = self.scotch()
+            result_dict = self.add_scotch(brand_s,size_s,wide,scale_s,colour_of_s,length,price_s)
+            return self.add_to_list(result_dict)
         return "Ви не можете додати товар такої категорії!"    
     
     def copy_paper(self):
@@ -81,15 +83,9 @@ class Store:
         price = input("Введіть вартість: ").capitalize()
         return brand,colour,format_of_paper,num_of_paper,price
         
-    
     def add_copy_paper(self,brand,colour,format_of_paper,num_of_paper,price):
         result_dict = {'категорія': "Папір офісний", 'торгова марка': f"{brand}", ' колір': f"{colour}", ' формат паперу': f"{format_of_paper}", 'кількість листів': f"{num_of_paper}",'вартість': f"{price}"}
-        if result_dict not in self.list_storage:
-            self.list_storage.append(result_dict)
-            Store.number_of_items +=1
-            return "Вітаю! Товар уcпішно додано"
-        else: 
-            return "Такий товар вже є!" 
+        return result_dict
         
     def for_table(self):
         brand_t = input("Введіть торгову марку: ").capitalize()
@@ -100,12 +96,7 @@ class Store:
     
     def add_for_table(self,brand_t,size,material,price_t):
         result_dict = {'категорія': "Настільне приладдя", 'торгова марка': f"{brand_t}", 'розмір': f"{size}", 'матеріал': f"{material}",'вартість': f"{price_t}"}
-        if result_dict not in self.list_storage:
-            self.list_storage.append(result_dict)
-            Store.number_of_items +=1
-            return "Вітаю! Товар уcпішно додано"
-        else: 
-            return "Такий товар вже є!" 
+        return result_dict
         
     def writing_instruments(self):
         brand_w = input("Введіть торгову марку: ").capitalize()
@@ -117,12 +108,7 @@ class Store:
         
     def add_writ_inst(self,brand_w,diametr,colour_of,price_w,line):
         result_dict = {'категорія': "Письмове приладдя", 'торгова марка': f"{brand_w}", 'діаметр стрижня': f"{diametr}", 'колір чорнила': f"{colour_of}", 'товщина лінії': f"{line}",'вартість': f"{price_w}"}
-        if result_dict not in self.list_storage:
-            self.list_storage.append(result_dict)
-            Store.number_of_items +=1
-            return "Вітаю! Товар уcпішно додано"
-        else: 
-            return "Такий товар вже є!"
+        return result_dict
         
     def scotch(self):
         brand_s = input("Введіть торгову марку: ").capitalize()
@@ -136,13 +122,7 @@ class Store:
         
     def add_scotch(self,brand_s,size_s,wide,scale_s,colour_of_s,length,price_s):
         result_dict = {'категорія': "Скотч, стрейч-плівка", 'торгова марка': f"{brand_s}", 'розмір': f"{size_s}", 'ширина': f"{scale_s}", 'колір': f"{colour_of_s}", 'довжина лінії': f"{length}",'вартість': f"{price_s}"}
-        if result_dict not in self.list_storage:
-            self.list_storage.append(result_dict)
-            Store.number_of_items +=1
-            return "Вітаю! Товар уcпішно додано"
-        else: 
-            return "Такий товар вже є!"
-        
+        return result_dict
          
     def num_of_items_func(self):
         return Store.number_of_items 
@@ -150,7 +130,6 @@ class Store:
     def all_items(self):
         for element in self.list_storage:
             print(element)
-   
     
     def del_func(self):
         del_categ = input("Введіть категорію товара,який хочете видалити: ").capitalize()
@@ -162,7 +141,6 @@ class Store:
                     if element['вартість'] == del_price:
                         self.list_storage.remove(element)
                         return "Успішно видалено!"
-                    
                     
     def search_func(self):
         search_categ = input("Введіть категорію товара,який хочете знайти: ").capitalize()
@@ -182,8 +160,16 @@ class Store:
                     if element['вартість'] == price:
                         element['коментарій'] = comment
                         return "Успішно додано!"
-    
-
+                    
+    def add_to_list(self, result_dict):
+        if result_dict not in self.list_storage:
+            self.list_storage.append(result_dict)
+            Store.number_of_items +=1
+            return "Вітаю! Товар уcпішно додано"
+        else: 
+            return "Такий товар вже є!" 
+        
+        
 hello(user_name()) 
 
 list_storage = []   
@@ -192,7 +178,7 @@ storage_filling(list_storage)
 items = Store(list_storage)   
 
 while True:
-    option = menu()
+    option = items.menu()
     if option == "q":
         print("OK,до зустрічі!")
         break    
