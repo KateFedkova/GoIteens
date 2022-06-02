e6a7eab58f7f475c6f88dadea1311d3cce46c6d0
KateFedkova <96693004+KateFedkova@users.noreply.github.com>
2022-06-02 13:16:21 +0300

    Update number_validation.py

--- code style and typing/number_validation.py
+++ code style and typing/number_validation.py
@@ -3,15 +3,11 @@ from typing import Any, Union, Tuple
 
 def number_validation(first_number: Any,
                       second_number: Any) -> \
-        Union[Tuple[int, int], Tuple[float, float], bool]:
+        Union[Tuple[float, float], bool]:
     '''Check if all numbers are digits'''
-    if first_number.isdigit() and second_number.isdigit():
-        return int(first_number), int(second_number)
-    elif float(first_number) and float(second_number):
-        return float(first_number), float(second_number)
-    else:
-        return False
-
+    if first_number.replace('.', '').isdigit and second_number.replace('.', '').isdigit():
+      return first_number, second_number
+    return False
 
 def numbers() -> Any:
     '''Input numbers'''
